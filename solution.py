962b116afabf42068583d04a8d872c5170a1b727
uilavrinenko <129886251+uilavrinenko@users.noreply.github.com>
2023-04-12 01:14:05 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,19 @@
 import pandas as pd
 import numpy as np
 
+import warnings
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+from hyppo.ksample import Energy, MMD, DISCO
+from scipy.stats import laplace, norm, ks_2samp, anderson_ksamp, cramervonmises_2samp
+from statsmodels.stats.weightstats import ztest
+from statsmodels.distributions.empirical_distribution import ECDF
+
+chat_id = 996494546 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    stat, pval = cramervonmises_2samp(x, y)
+    
+    return pval < 0.03 # Ваш ответ, True или False
